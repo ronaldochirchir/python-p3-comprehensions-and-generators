c94a037c6721cf9d3aff6e9cedda8c3fe9423767
ronaldochirchir <ronaldochirchir8@gmail.com>
2025-02-12 23:58:52 +0300

    completed lap

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
-
 def return_evens(num_list):
-    pass
+    # Return a list of even numbers using a list comprehension
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    # Return a list of sentences with exclamation marks using a list comprehension
+    return [sentence + "!" for sentence in sentence_list]
